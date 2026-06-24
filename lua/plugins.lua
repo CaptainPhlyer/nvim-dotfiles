d78199312a4ea963b7f374f8ccfbf9e62f2f3fd9
Ben <ben@Bens-MacBook-Air.local>
2026-06-24 13:34:26 +0200

    change plugins + config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -221,7 +221,7 @@ require("lazy").setup({
   },
   {
     "nvim-neorg/neorg",
-	dependencies = {"luarocks.nvim","plenary.nvim"},
+	dependencies = {"luarocks.nvim","plenary.nvim","nvim-neorg/tree-sitter-norg"},
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
 	config = function()
@@ -233,9 +233,9 @@ require("lazy").setup({
             ["core.dirman"] = {
               config = {
                 workspaces = {
-                  notes = "~/notes",
+                  pf = "~/notes/pathfinder-world/",
                 },
-                default_workspace = "notes",
+                default_workspace = "pf",
               },
             },
           },
